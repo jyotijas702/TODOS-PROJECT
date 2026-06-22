3a9da4d2b41e715cbe964941d0ae71870d310762
Jyoti Kumari <1996.jyotisuman@gmail.com>
2026-06-22 18:26:54 +0530

    added inital ui

---
+++ FrontEnd/script.js
@@ -0,0 +1,52 @@
+const todos = [
+  {
+    task: "Go for walk",
+    isDone: false,
+    createdAt: new Date(),
+    id: "1tsegfc",
+  },
+  {
+    task: "Do Yoga",
+    isDone: true,
+    createdAt: new Date("2026-06-21"),
+    id: "2hncg",
+  },
+  {
+    task: "Drink Water",
+    isDone: true,
+    createdAt: new Date("2026-06-20"),
+    id: "3vfredc",
+  },
+];
+
+const todoListEl = document.querySelector("#todo-list");
+todos.forEach((todo) => {
+  const todoEl = document.createElement("li");
+  todoEl.className = "todo-item";
+
+  const todoTitleEl = document.createElement("p");
+  todoTitleEl.innerText = todo.task;
+  todoTitleEl.className = "todo-title";
+
+  const finishBtn = document.createElement("button");
+  finishBtn.innerHTML = `<span class="finish-btn">Finish</span>✓`;
+  finishBtn.dataset.todoId = todo.id;
+
+  const undoBtn = document.createElement("button");
+  undoBtn.innerHTML = `<span class="undo-btn">Undo</span>↩`;
+  undoBtn.dataset.todoId = todo.id;
+
+  const deleteBtn = document.createElement("button");
+  deleteBtn.innerHTML = `<span class="delete-btn">Delete</span>❌`;
+  deleteBtn.dataset.todoId = todo.id;
+
+  todoEl.appendChild(todoTitleEl);
+  if (todo.isDone) {
+    todoEl.appendChild(undoBtn);
+  } else {
+    todoEl.appendChild(finishBtn);
+  }
+  todoEl.appendChild(deleteBtn);
+
+  todoListEl.appendChild(todoEl);
+});
