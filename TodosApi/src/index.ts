297e471794c3b5afbc8a4971ad5ed0428e1f9e08
Jyoti Kumari <1996.jyotisuman@gmail.com>
2026-06-25 16:33:11 +0530

    added todos get api

--- TodosApi/src/index.ts
+++ TodosApi/src/index.ts
@@ -1,12 +1,22 @@
 import express, { Application, Request, Response } from "express";
 import mongoose from "mongoose";
 import { MONGO_URL, PORT } from "./constants";
+import { TodoModel } from "./db/todo";
 
 const app: Application = express();
 app.use(express.json());
 
 app.get("/", (req: Request, res: Response) => {
   res.json({ message: "Hello from Express with TypeScript!" });
+});
+
+app.get("/todos", async (req: Request, res: Response) => {
+  const todos = await TodoModel.find({}).select([
+    "task",
+    "isDone",
+    "createdAt",
+  ]);
+  res.json(todos);
 });
 
 mongoose
